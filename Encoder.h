bcc7092160d58519164aac9af2f763001950573b
dxdsun <daniel_sun1001@yahoo.com>
2014-05-02 00:19:35 -0700

    Update Encoder.h

--- Encoder.h
+++ Encoder.h
@@ -1,6 +1,19 @@
 #ifndef ENCODER_H
 #define ENCODER_H
 
+#include <stdint.h>
+
+#define Lencoder 1
+#define Rencoder 2
+#define Rotation 24
+
+void initEncoders(uint8_t outA1, uint8_t outB1, uint8_t outA2, uint8_t outB2);
+void iterate1();
+void iterate2();
+void iterate3();
+void iterate4();
+void resetCounter(uint8_t encoder);
+
 
 
 #endif // ENCODER_H
