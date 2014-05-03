eb823acb1c5463d33a5d4fed92e90b5c55193564
Alvin Ho <Alvin.Ho@ucla.edu>
2014-05-03 03:46:44 -0700

    Maze solving algorithm implemented.

---
+++ Solver.cpp
@@ -0,0 +1,67 @@
+#include "Solver.h"
+#include <iostream>
+
+uint8_t maze[16][16];
+uint8_t wallsx[15][16];
+uint8_t wallsy[16][15];
+
+void initializeWalls() {
+    int i, j;
+    for(i = 0; i < 16; i++)
+        for(j = 0; j < 16; j++)
+            maze[i][j] = 0;
+    for(i = 0; i < 15; i++)
+        for(j = 0; j < 16; j++)
+            wallsx[i][j] = 0;
+    for(i = 0; i < 16; i++)
+        for(j = 0; j < 16; j++)
+            wallsy[i][j] = 0;
+}
+
+Heading headingLeft(Heading hd) {
+    switch(hd) {
+      case North:
+          return West;
+      case South:
+          return East;
+      case East:
+          return North;
+      case West:
+          return South;
+    }
+}
+
+void insertWall(Heading hd, Point pos) {
+    x = pos.x;
+    y = pos.y;
+    switch(hd) {
+      case North:
+          wallsy[x][y] = 1;
+      case South:
+          wallsy[x][y-1] = 1;
+      case East:
+          wallsx[x][y] = 1;
+      case West:
+          wallsx[x-1][y] = 1;
+    }
+}
+
+void updateWalls(bool left, bool right, bool front, Point pos, Heading hd) {
+    Heading cur;
+    if(left) {
+        if(pos.x = 0)
+            break;
+        cur = headingLeft(hd);
+        insertWall(cur);
+    }
+}
+
+int main() {
+    Point p;
+    p.x = 1;
+    p.y = 1;
+    updateWalls(true, false, false, p, North);
+    return 1;
+}
+
+
