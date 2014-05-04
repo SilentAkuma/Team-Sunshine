0f4db0dfdda7d711d5da04d7cf6aa74493449949
Alvin Ho <Alvin.Ho@ucla.edu>
2014-05-03 23:18:26 -0700

    Updated versions of the solver.  Added function that returns vector of next location.

--- Solver.h
+++ Solver.h
@@ -1,22 +1,14 @@
-#ifndef SOLVER_H
-#define SOLVER_H
-
-#include <stdint.h>
-
-typedef struct
-{
+typedef struct Point {
     uint8_t x;
     uint8_t y;
 } Point;
 
-typedef enum
-{
+enum Heading {
     North,
     South,
     East,
     West
-} Heading;
+};
 
-void updateWalls(bool left, bool right, bool front, Point pos, Heading hd);
+updateWalls(bool left, bool right, bool front, Point pos, Heading hd);
 
-#endif // SOLVER_H
